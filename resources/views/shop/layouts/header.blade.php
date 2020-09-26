98bf541b714da066e8f1074724b71345bc7622e3
Tcharles Pereira da Silva <tcharlezin@gmail.com>
2020-09-26 17:29:07 -0300

    Subindo carrinho com produtos

--- resources/views/shop/layouts/header.blade.php
+++ resources/views/shop/layouts/header.blade.php
@@ -47,39 +47,47 @@
                             <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
                                 <i class="fa fa-shopping-cart"></i>
                                 <span>Your Cart</span>
-                                <div class="qty">3</div>
+                                <div class="qty">2</div>
                             </a>
                             <div class="cart-dropdown">
-                                <div class="cart-list">
-                                    <div class="product-widget">
-                                        <div class="product-img">
-                                            <img src="./img/product01.png" alt="">
-                                        </div>
-                                        <div class="product-body">
-                                            <h3 class="product-name"><a href="#">product name goes here</a></h3>
-                                            <h4 class="product-price"><span class="qty">1x</span>$980.00</h4>
-                                        </div>
-                                        <button class="delete"><i class="fa fa-close"></i></button>
-                                    </div>
 
-                                    <div class="product-widget">
-                                        <div class="product-img">
-                                            <img src="./img/product02.png" alt="">
+                                @php
+                                    $products = \App\Models\Catalog\Product::all()->random(3);
+                                @endphp
+
+                                <div class="cart-list">
+
+                                    @foreach($products as $product)
+
+                                        @php
+                                            $productInformation = (new \App\Dominio\Catalog\ProductInformation($product->id))->get();
+
+                                            $categoryName = $category->name;
+                                            $name = $productInformation["name"];
+                                            $productImage = $productInformation["images"][0];
+                                            $productPrice = $productInformation["price"][0];
+                                        @endphp
+
+                                        <div class="product-widget">
+                                            <div class="product-img">
+                                                <img src="{{ $productImage }}" alt="">
+                                            </div>
+                                            <div class="product-body">
+                                                <h3 class="product-name"><a href="#">{{ $product->name }}</a></h3>
+                                                <h4 class="product-price"><span class="qty">1x</span>${{ $productPrice }}</h4>
+                                            </div>
+                                            <button class="delete"><i class="fa fa-close"></i></button>
                                         </div>
-                                        <div class="product-body">
-                                            <h3 class="product-name"><a href="#">product name goes here</a></h3>
-                                            <h4 class="product-price"><span class="qty">3x</span>$980.00</h4>
-                                        </div>
-                                        <button class="delete"><i class="fa fa-close"></i></button>
-                                    </div>
+                                    @endforeach
+
                                 </div>
                                 <div class="cart-summary">
                                     <small>3 Item(s) selected</small>
                                     <h5>SUBTOTAL: $2940.00</h5>
                                 </div>
                                 <div class="cart-btns">
-                                    <a href="#">View Cart</a>
-                                    <a href="#">Checkout  <i class="fa fa-arrow-circle-right"></i></a>
+                                    <a href="{{route("shop.checkout")}}">View Cart</a>
+                                    <a href="{{route("shop.checkout")}}">Checkout  <i class="fa fa-arrow-circle-right"></i></a>
                                 </div>
                             </div>
                         </div>
