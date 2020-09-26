84ec349005ef8fc48222a48f1ab67280d0708051
Maru Amallo <maruamallo@gmail.com>
2020-09-26 19:49:46 -0300

    fix> team name

--- resources/views/shop/layouts/app.blade.php
+++ resources/views/shop/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>#WeArBrDevsHackathon</title>
+    <title>#WeBrArDevsHackathon</title>
 
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
     <link href="{{ asset("shop/css/bootstrap.min.css") }}" rel="stylesheet">
