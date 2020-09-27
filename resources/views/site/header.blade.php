118778b30f76e759deb262f2189503ace3e41a1c
Tcharles Pereira da Silva <tcharlezin@gmail.com>
2020-09-27 05:09:10 -0300

    Removing login and create account

--- resources/views/site/header.blade.php
+++ resources/views/site/header.blade.php
@@ -14,18 +14,6 @@
                         <span class="header-slogan-text">{{env("APP_SLOGAN")}}</span>
                     </div>
                 </div>
-                <ul class="header-actions nav nav-pills">
-                    @if( Auth::guest())
-                        <li><a href="#" id="btnLogin">Login</a></li>
-                        <li><a href="#" id="btnCriarConta" class="primary">Create a acount</a></li>
-                    @else
-                        <li>
-                            <div style="position: relative; display: block;">
-                                <a href="{{route("home")}}" class="btn btn-default">Entrar</a>
-                            </div>
-                        </li>
-                    @endif
-                </ul>
                 <button class="navbar-toggle collapsed" type="button" data-toggle="collapse"
                         data-target=".header-nav">
                     <span class="sr-only">Toggle navigation</span>
@@ -57,9 +45,6 @@
                         @endif
                     </ul>
                     <div class="header-search hidden-sm">
-                        <form method="get" action="?">
-                            <input type="text" class="form-control" placeholder="Search ...">
-                        </form>
                     </div>
                 </div>
             </div>
