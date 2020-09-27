e3eb79cb1a68ed79855ea64b6fe964ed52d2b61d
Maru Amallo <maruamallo@gmail.com>
2020-09-26 23:05:17 -0300

    add index into sku table

---
+++ database/migrations/2020_09_27_020327_add_index_into_sku_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIndexIntoSkuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('skus', function (Blueprint $table) {
+            $table->index('code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
