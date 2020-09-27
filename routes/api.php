39bec042cae526d7d3782a409b83da11aa6a58cb
Maru Amallo <maruamallo@gmail.com>
2020-09-26 23:07:14 -0300

    api endpoint for recomendation

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/recomend', '\App\Http\Controllers\Shop\ProductController@recomendationApi')->name("api.recomend");
